3330a37d6c40743932c801f5f61ecc5f9c71be78
Perrie Macdonald <pmacdonald@tonkintaylor.co.nz>
2023-12-15 15:16:51 +1300

    data processing

--- data_processing.py
+++ data_processing.py
@@ -1,3 +1,4 @@
+import openpyxl
 import pandas as pd
 
 # read in csv file
@@ -16,13 +17,13 @@ df_names = df_large.groupby('Date')[bool_columns].sum().reset_index()
 df_tidy = pd.merge(df, df_names, on='Date', how='left')
 
 # drop irrelevant columns
-cols_to_drop = ['Participants (number, office, charge out rate)', 'TBC', 'TBC with Keith', '', 'Unnamed: 10', "Can't remember"]
+cols_to_drop = ['Participants (number, office, charge out rate)', 'TBC', 'TBC with Keith', '', 'Unnamed: 10', "Can't remember", "Can't remember"]
 df_tidy = df_tidy.drop(columns=cols_to_drop)
 df_tidy = pd.merge(df, df_names, on='Date', how='left')
 print(df_tidy.columns)
 
 # add weather data
-weather_df = pd.read_csv('weather_data.csv')
+weather_df = pd.read_csv('full_weather_data.csv')
 weather_df['datetime'] = pd.to_datetime(weather_df['datetime'], format='mixed',dayfirst=True)
 weather_df.rename(columns={'datetime': 'Date'}, inplace=True)
 
@@ -31,7 +32,23 @@ df_tidy = pd.merge(df_tidy, weather_df, on='Date', how='left')
 # add staff info (charge out rate, employee #)
 staff_df = pd.read_excel('staff_info.xlsx')
 
+# Initialize the total charge out rate column
+df_tidy['Total_Charge_out_rate'] = 0
 
+# Iterate over staff columns and merge data
+for staff_column in df_tidy.columns[12:52]:
+    # Extract staff number from column name
+    matched_row = staff_df[staff_df['Name'].str.contains(staff_column, case=False)]
+    if matched_row.empty:
+        rate = 0
+    else:
+        rate = matched_row.iloc[0, 8]
+
+    # Sum charge out rates for each meeting
+    df_tidy['Total_Charge_out_rate'] += df_tidy[staff_column]*rate
+
+# Display the result
+print(df_tidy)
 
 
 
